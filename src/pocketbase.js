1256ee1692049c8db3c5f2a0bc35a51f7216b512
unknown <lzoussama003@gmail.com>
2026-05-24 18:54:22 +0200

    use env variable for pocketbase

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,4 +1,4 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase(import.meta.env.VITE_PB_URL);
+export const pb = new PocketBase("https://stamp-payment-responses-calculations.trycloudflare.com");
 pb.autoCancellation(false);
