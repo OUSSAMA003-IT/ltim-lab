412ba31c77ed5b52c53e7f7757949d82b09f6f92
unknown <lzoussama003@gmail.com>
2026-05-29 15:34:54 +0200

    use tunnel backend url

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,4 +1,4 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase(import.meta.env.VITE_PB_URL);
+export const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
 pb.autoCancellation(false);
