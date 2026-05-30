81423f08301d27eb5d608730e9a568400aa63902
unknown <lzoussama003@gmail.com>
2026-05-30 19:33:50 +0000

    configure pocketbase url

--- src/pocketbase.js
+++ src/pocketbase.js
@@ -1,5 +1,5 @@
 import PocketBase from "pocketbase";
 
-export const pb = new PocketBase("import.meta.env.VITE_POCKETBASE_URL");
+export const pb = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
 
 pb.autoCancellation(false);
